4973bcc137da3be76ed52d9541d5e38d2e1622c5
Krishun Marqru <krishun@Pamelas-MacBook-Air.local>
2017-01-06 13:28:55 -0500

    create card w/javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,30 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardFour===cardThree || cardOne===cardTwo){
 	alert('You found a match.');
 } else if (cardOne===cardThree || cardTwo===cardFour){
 	alert('Sorry, try again.');
+}*/
+
+var createCards= function(){
+var gameBoard= document.getElementById('game-board');
+
+for (var i=1; i<=4; i+=1){
+
+	var newCard= document.createElement('div');
+	newCard.className= 'card';
+
+	gameBoard.appendChild(newCard);
+	}
 }
+createCards();
+
+
+
+
+
+
+
+
